8ad14b6b8e3c8c8168820d9b89b87ea94265b70e
Ashu Jha <ashujha009322@gmail.com>
2025-11-15 19:59:41 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+
+module.exports = withSentryConfig(nextConfig, {
+  org: "example-org",
+  project: "sports",
+  // Only print logs for uploading source maps in CI
+  // Set to `true` to suppress logs
+  silent: !process.env.CI,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+});
